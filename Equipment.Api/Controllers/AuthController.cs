e51024d51fa42807176961547c8ca27782bdc1db
JustIlnaz <ilnazikwork@gmail.com>
2026-07-04 11:41:07 +0300

    project done

--- Equipment.Api/Controllers/AuthController.cs
+++ Equipment.Api/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
+using System.Security.Claims;           
 using System.Text;
 
 namespace Equipment.Api.Controllers
